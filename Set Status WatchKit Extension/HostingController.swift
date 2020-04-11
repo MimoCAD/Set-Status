7c0ac21327715368b66d3c637b0b57008839beac
Mark Tomlin <Mark@MimoCAD.io>
2020-04-10 22:07:15 -0400

    Initial Commit

---
+++ Set Status WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Set Status WatchKit Extension
+//
+//  Created by Mark Tomlin on 4/10/20.
+//  Copyright © 2020 MimoCAD Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Set Status WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Set Status WatchKit Extension
+//
+//  Created by Mark Tomlin on 4/10/20.
+//  Copyright © 2020 MimoCAD Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
