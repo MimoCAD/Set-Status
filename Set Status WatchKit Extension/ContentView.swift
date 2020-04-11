8880b5f2ca1c511939d417c5ddc30b36a91c7f66
Mark Tomlin <Mark@MimoCAD.io>
2020-04-10 23:00:42 -0400

    First Build of Set Status

--- Set Status WatchKit Extension/ContentView.swift
+++ Set Status WatchKit Extension/ContentView.swift
@@ -9,8 +9,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var EnrouteText = "Enroute"
+    @State var OnSceneText = "On Scene"
+    @State var LeaveSceneText = "Leave Scene"
+    @State var AtHospitalText = "At Hospital"
+    @State var InServiceText = "In Service"
+
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Button(action: {
+                self.EnrouteText = getCurrentTime()
+            }) {
+                Text(EnrouteText)
+            }
+            .foregroundColor(.red)
+
+            Button(action: {
+                self.OnSceneText = getCurrentTime()
+            }) {
+                Text(OnSceneText)
+            }.foregroundColor(.yellow)
+
+            Button(action: {
+                self.LeaveSceneText = getCurrentTime()
+            }) {
+                Text(LeaveSceneText)
+            }.foregroundColor(.green)
+
+            Button(action: {
+                self.AtHospitalText = getCurrentTime()
+            }) {
+                Text(AtHospitalText)
+            }.foregroundColor(.blue)
+
+            Button(action: {
+                self.InServiceText = getCurrentTime()
+            }) {
+                Text(InServiceText)
+            }.foregroundColor(.gray)
+
+            Button(action: {
+                self.EnrouteText = "Enroute"
+                self.OnSceneText = "On Scene"
+                self.LeaveSceneText = "Leave Scene"
+                self.AtHospitalText = "At Hospital"
+                self.InServiceText = "In Service"
+            }) {
+                Text("Reset")
+            }
+        }
+        .navigationBarTitle("Times")
     }
 }
 
@@ -18,4 +66,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func getCurrentTime() -> String {
+    let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "HH:mm:ss"
+
+    return dateFormatter.string(from: Date())
 }
